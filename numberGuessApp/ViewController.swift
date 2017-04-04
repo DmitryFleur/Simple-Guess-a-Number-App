1534d7b11dc3dc671adb8b32cd88f2456e614bcd
DmitryFleur <dimitrik.ko@gmail.com>
2017-04-04 10:48:26 +0300

    Simple Guess a Number App
    Simple “Guess a Number App“ made in swift, using Constraints

--- numberGuessApp/ViewController.swift
+++ numberGuessApp/ViewController.swift
@@ -9,15 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var numberGenerator : Int = 0;
 
+    @IBOutlet weak var userNumber: UITextField!
+    @IBOutlet weak var Result: UILabel!
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        numberGenerator = Int(arc4random_uniform(11))
+        print(numberGenerator)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func submitButton(_ sender: Any) {
+        if Int(userNumber.text!)! > numberGenerator{
+            Result.text = "Number > x"
+        }
+        else if Int(userNumber.text!)! < numberGenerator{
+            Result.text = "Number < x"
+        }
+        else{
+            Result.text = "You won"
+        }
+
+        
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
     }
 
 
